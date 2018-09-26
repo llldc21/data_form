c6f04a0574780186e8b3fa6d882711dd0533aa54
Lucas Lima <lucas@digitalmap.com.br>
2018-09-26 09:55:26 -0300

    Atualizando banco de dados e formulario

--- back_end/conexao.php
+++ back_end/conexao.php
@@ -7,8 +7,4 @@ $conn->query("SET NAMES 'utf8'");
 $conn->query('SET character_set_connection=utf8');
 $conn->query('SET character_set_client=utf8');
 $conn->query('SET character_set_results=utf8');
-
-if ($conn) {
-    echo 'ok!';
-}
 ?>
