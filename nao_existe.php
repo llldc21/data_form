ede27d94dc9e9da0a977dc2b9f2bc40f858786a6
Mark <marcos.silva983@etec.sp.gov.br>
2018-12-07 22:58:38 +0000

    estilizando a pagina nao_existe.php

--- nao_existe.php
+++ nao_existe.php
@@ -2,9 +2,37 @@
 include('navbar.php');
 session_start();
 ?>
-  
-  <h1> Formulario não Existe</h1>
+<style>
+  i {
+    text-align:center;
+    }
+  h9 {
+    font-size:120px;
+    font-weight:lighter;
+  }
+  p {
+    font-size:25px;
+    
+  }
+  .sad {
+    margin-top:40px;
+    font-size:32px;
+    text-align:center;
+  }
+</style>
+
   </nav>
 <body>
-    
+  <div class="row">
+    <div class="col-md-12">
+      <div class="row sad">
+        <div class="col-md-12">
+            <i class="far fa-frown fa-9x"></i>
+            <br>
+            <h9>404</h9>
+          
+            <p>O Formulário que você esta procurando não existe ou foi fechado.</p>
+         </div>
+    </div>
+  </div>
 </html>
