bdacd470318fd15a0931a1ba9516d14f142e7a74
Mark <marcos.silva983@etec.sp.gov.br>
2018-12-07 23:02:26 +0000

    estilizando a pagina nao_existe.php

--- navbar.php
+++ navbar.php
@@ -16,8 +16,6 @@
   <!-- Bootstrap core CSS -->
   <link href="front_end/temas/startbootstrap-one-page-wonder-gh-pages/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
   <!-- Custom fonts for this template -->
   <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
@@ -26,6 +24,9 @@
   <link rel="stylesheet" href="front_end/css/main.css">
   <link rel="stylesheet" href="front_end/css/pes.css">
   <link href="front_end/temas/startbootstrap-one-page-wonder-gh-pages/css/one-page-wonder.css" rel="stylesheet">
+  
+  <!-- FONT AWSOME -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 </head>
 
